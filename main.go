3ec7194a744677a35171f14540dbc50f7e7261ca
Ibrahim Mbaziira <code.ibra@gmail.com>
2024-03-18 14:24:33 +0300

    fetch key values concurrently
    Signed-off-by: Ibrahim Mbaziira <code.ibra@gmail.com>

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -19,7 +20,7 @@ type Data struct {
 func main() {
 	ctx := context.Background()
 	rdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"localhost:6376", "localhost:6375"},
+		Addrs: []string{"localhost:7002", "localhost:7003"},
 	})
 
 	_, err := rdb.Ping(ctx).Result()
@@ -39,12 +40,35 @@ func main() {
 	defer file.Close()
 
 	writer := csv.NewWriter(file)
-	defer func() {
-		writer.Flush()
+	defer writer.Flush()
+
+	var wg sync.WaitGroup
+	dataChannel := make(chan Data)
+	errChannel := make(chan error, 1)
+	doneChannel := make(chan bool)
+
+	go func() {
+		var n int
+		for data := range dataChannel {
+			if n == 0 {
+				headers := []string{"assignmentDate", "isExcluded"}
+				if err := writer.Write(headers); err != nil {
+					errChannel <- err
+					return
+				}
+			}
+			record := []string{data.AssignmentDate, fmt.Sprintf("%t", data.IsExcluded)}
+			if err := writer.Write(record); err != nil {
+				errChannel <- err
+				return
+			}
+			n++
+		}
+		doneChannel <- true
 	}()
 
 	var cursor uint64
-	var n int
+	cap := make(chan struct{}, 50)
 	for {
 		var keys []string
 		keys, cursor, err = rdb.Scan(ctx, cursor, "*", 0).Result()
@@ -54,33 +78,29 @@ func main() {
 		}
 
 		for _, key := range keys {
-			val, err := rdb.Get(ctx, key).Result()
-			if err != nil {
-				fmt.Println("error getting value for key:", key, err)
-				continue
-			}
-
-			var data Data
-			err = json.Unmarshal([]byte(val), &data)
-			if err != nil {
-				fmt.Println("error unmarshalling json:", err)
-				continue
-			}
-
-			if data.AssignmentDate == yesterday && data.IsExcluded {
-				if n == 0 {
-					headers := []string{"assignmentDate", "isExcluded"}
-					if err := writer.Write(headers); err != nil {
-						fmt.Println("nth error:", err)
-					}
+			wg.Add(1)
+			cap <- struct{}{}
+			go func(key string) {
+				defer wg.Done()
+				val, err := rdb.Get(ctx, key).Result()
+				if err != nil {
+					fmt.Println("error getting value for key:", key, err)
+					<-cap
+					return
 				}
 
-				record := []string{data.AssignmentDate, fmt.Sprintf("%t", data.IsExcluded)}
-				if err := writer.Write(record); err != nil {
-					fmt.Println("record error:", err)
+				var data Data
+				if err := json.Unmarshal([]byte(val), &data); err != nil {
+					fmt.Println("error unmarshalling json:", err)
+					<-cap
+					return
 				}
-				n++
-			}
+
+				if data.AssignmentDate == yesterday && data.IsExcluded {
+					dataChannel <- data
+				}
+				<-cap
+			}(key)
 		}
 
 		if cursor == 0 {
@@ -88,5 +108,12 @@ func main() {
 		}
 	}
 
-	fmt.Printf("csv file %s created, %d records written.\n", fileName, n)
+	wg.Wait()
+	close(dataChannel)
+	select {
+	case <-doneChannel:
+		fmt.Printf("csv file %s created.\n", fileName)
+	case err := <-errChannel:
+		fmt.Printf("error writing to csv: %v\n", err)
+	}
 }
